00aabe26346ba253df553d18fd847c7aac240d64
xfnw <xfnw@ttm.sh>
2020-07-24 01:31:31 +0000

    stricter invite ignoring

--- modules/invite.py
+++ modules/invite.py
@@ -1,10 +1,13 @@
 import time
 
 async def invite(self, channel, by):
+
+    if self.db['invite'].find_one(blacklist=channel):
+        print('{} invited me to {}, a blacklisted channel'.format(by,channel))
+        return
+
     if self.db['invite'].find_one(enabled='true'):
-        if self.db['invite'].find_one(blacklist=channel):
-            print('{} invited me to {}, a blacklisted channel'.format(by,channel))
-            return
+
         print('{} invited me to {}!'.format(by, channel))
         self.t = time.time()+1
         await self.join(channel)
